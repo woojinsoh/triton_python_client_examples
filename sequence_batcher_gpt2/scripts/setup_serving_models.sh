1b692c40962c5eab7db90ee7ce45c70e715a35ac
Woojin Soh <wujinish@gmail.com>
2022-06-08 06:41:16 +0000

    Adding lines to make model repository sub-folders

--- sequence_batcher_gpt2/scripts/setup_serving_models.sh
+++ sequence_batcher_gpt2/scripts/setup_serving_models.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/sh
 mkdir -p persistence
+mkdir -p models/ensemble_pipeline/1
+mkdir -p models/generator_onnx/1
+mkdir -p models/generator_torchscript/1
 
 python3 utils/get_gpt2.py
 cp persistence/model.pt models/generator_torchscript/model.pt
